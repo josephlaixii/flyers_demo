740a1fee3ccb8e79d87dacf97e41f5f7ba1ba7f6
joseph <josephlaixii@gmail.com>
2016-12-05 23:08:21 -0500

    added page number

--- app/src/main/java/com/flyers/db_software_incorporateion/db_flyers/ViewPagerActivity.java
+++ app/src/main/java/com/flyers/db_software_incorporateion/db_flyers/ViewPagerActivity.java
@@ -33,6 +33,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -46,9 +48,15 @@ public class ViewPagerActivity extends AppCompatActivity {
 	private ViewPager mViewPager;
 	private ArrayList<Bitmap> arrBitMap = new ArrayList<>();
 	private ArrayList<String> gurlUpdate = new ArrayList<>();
+	private ArrayList<String> gurlUpdate2 = new ArrayList<>();
 	private ArrayList<String> expiredate = new ArrayList<>();
-	private String title;
+	private String title,src3;
 	private int i;
+	public static Toolbar toolbar;
+	OkHttpClient client,client2;
+	Request request,request2;
+
+	Response response2,response3;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,13 +65,14 @@ public class ViewPagerActivity extends AppCompatActivity {
 
 
 
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		 toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
 		Bundle extra3 = getIntent().getBundleExtra("btitle");
 		title = extra3.getString("btitle");
 
 		getSupportActionBar().setTitle(title);
+
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setDisplayShowHomeEnabled(true);
 
@@ -78,6 +87,7 @@ public class ViewPagerActivity extends AppCompatActivity {
 
 		if (!gurlUpdate.isEmpty()) {
 			new ImageLoadTask(gurlUpdate.get(i)).execute();
+			System.out.println("new ImageLoadTask(gurlUpdate.get(i)).execute()2" + gurlUpdate.get(i));
 		}
 	}
 
@@ -115,12 +125,14 @@ public class ViewPagerActivity extends AppCompatActivity {
 		protected Bitmap doInBackground(Void... params) {
 			try {
 
-				OkHttpClient client = new OkHttpClient();
-				Request request = new Request.Builder()
-						.url(url)
-						.build();
+				if(!url.equals("")) {
+					client = new OkHttpClient();
+					request = new Request.Builder()
+							.url(url)
+							.build();
 
-				Response response2 = client.newCall(request).execute();
+					response2 = client.newCall(request).execute();
+				}
 
 				if(response2.code() == 200) {
 
@@ -131,27 +143,77 @@ public class ViewPagerActivity extends AppCompatActivity {
 							.referrer("http://www.google.com")
 							.get();
 
-					Elements img = document.getElementsByTag("img");
 
-
-					for (Element el : img) {
-						src = el.absUrl("src");
-						URL newurl = new URL(src);
-						myBitmap = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
-						arrBitMap.add(myBitmap);
-					}
-
-					int i2[] = {0, 1, arrBitMap.size() - 1};
-					int ilen = i2.length;
-					for (int j = 0; j < ilen; j++) {
-						arrBitMap.remove(i2[j] - j);
-					}
 
 					Elements div = document.select("div.flyer-card");
 					Elements flyercont = div.select("div.flyer-content");
 					Elements flyerstore = flyercont.select("div.flyer-store");
 					Elements flyerstoreinner = flyerstore.select("div.flyer-store-inner");
 					Elements span = flyerstoreinner.select("span");
+
+					Elements a = flyercont.select("a");
+
+					for (Element el : a) {
+						src3 = el.absUrl("href");
+						gurlUpdate2.add(src3);
+
+					}
+
+
+					if (!gurlUpdate2.isEmpty()) {
+
+						client2 = new OkHttpClient();
+						request2 = new Request.Builder()
+								.url(gurlUpdate2.get(0))
+								.build();
+
+						response3 = client2.newCall(request2).execute();
+					}
+
+					if (response3.code() == 200) {
+
+						Document document2 = Jsoup.connect(gurlUpdate2.get(0)+"/all").header("Accept-Encoding", "gzip, deflate")
+								.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
+								.maxBodySize(0)
+								.method(org.jsoup.Connection.Method.GET)
+								.referrer("http://www.google.com")
+								.get();
+
+
+
+						Elements img = document2.getElementsByTag("img");
+
+
+						for (Element el : img) {
+							src = el.absUrl("src");
+
+
+
+							URL url = new URL(src);
+							HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+							connection.setDoInput(true);
+							connection.connect();
+							InputStream input = connection.getInputStream();
+
+							BitmapFactory.Options options = new BitmapFactory.Options();
+							options.inJustDecodeBounds = true;
+							myBitmap = BitmapFactory.decodeStream(input);
+							int imageHeight = options.outHeight;
+							int imageWidth = options.outWidth;
+							String imageType = options.outMimeType;
+
+							arrBitMap.add(myBitmap);
+						}
+
+
+						int i2[] = {0, 1, arrBitMap.size() - 1};
+						int ilen = i2.length;
+						for (int j = 0; j < ilen; j++) {
+							arrBitMap.remove(i2[j] - j);
+						}
+
+
+					}
 
 					for (Element el: span){
 						expiredate.add(el.text());
@@ -170,11 +232,14 @@ public class ViewPagerActivity extends AppCompatActivity {
 		@Override
 		protected void onPostExecute(Bitmap result) {
 			super.onPostExecute(result);
-
+			myBitmap.recycle();
+			myBitmap=null;
 			if(!expiredate.isEmpty()) {
 				Toolbar toolbarbottom = (Toolbar) findViewById(R.id.toolbar_bottom);
 				setSupportActionBar(toolbarbottom);
 				getSupportActionBar().setTitle(expiredate.get(0));
+				setSupportActionBar(toolbar);
+
 			}
 
 			mViewPager.setAdapter(new SamplePagerAdapter(result,arrBitMap));
@@ -183,15 +248,17 @@ public class ViewPagerActivity extends AppCompatActivity {
 
 	}
 
-	static class SamplePagerAdapter extends PagerAdapter {
+	 class SamplePagerAdapter extends PagerAdapter {
 
 
 		private Bitmap bitmap;
 		private ArrayList<Bitmap> sarrBitMap = new ArrayList<>();
-
+		private Toolbar toolbar;
+		 private int ipos=0;
 		public SamplePagerAdapter(Bitmap bitmap, ArrayList<Bitmap> sarrBitMap) {
 			this.bitmap = bitmap;
 			this.sarrBitMap =sarrBitMap;
+
 		}
 
 		@Override
@@ -202,18 +269,27 @@ public class ViewPagerActivity extends AppCompatActivity {
 		@Override
 		public View instantiateItem(ViewGroup container, int i) {
 			PhotoView photoView = new PhotoView(container.getContext());
+			toolbar = ViewPagerActivity.toolbar;
 			//photoView.setImageResource(sDrawables[position]);
+			getSupportActionBar().setSubtitle(i + " of " + (sarrBitMap.size()-1));
 			if(!sarrBitMap.isEmpty()) {
 				photoView.setImageBitmap(sarrBitMap.get(i));
+
+
 				// Now just add PhotoView to ViewPager and return it
 			}
+
+
+
+			getSupportActionBar().setSubtitle(mViewPager.getCurrentItem()+1 + " of " + (sarrBitMap.size()));
 			container.addView(photoView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 
 			return photoView;
 		}
 
 		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
+		public void destroyItem(ViewGroup container, int i, Object object) {
+			getSupportActionBar().setSubtitle(mViewPager.getCurrentItem()+1 + " of " + (sarrBitMap.size()));
 			container.removeView((View) object);
 		}
 
